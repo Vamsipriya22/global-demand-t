ad6c456c17b78022ec91a1d87e50c4c71a51f95e
Vamsipriya22 <golivamsipriya22@gmail.com>
2025-04-24 21:46:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,8 +46,9 @@ const countryData = {
     "Argentina": { "color": "#CC9933", "link": "https://api.cammesa.com/demanda-svc/swagger-ui.html#/demanda-ws" },
     "Brazil": { "color": "#33FFCC", "link": "https://dados.ons.org.br/dataset/curva-carga" },
     "Canada": { "color": "#FF6F61", "link": "https://energy-information.canada.ca/en/resources/high-frequency-electricity-data"},
-    "Chile": { "color": "#FFCC99", "link": "https://www.coordinador.cl/operacion/graficos/operacion-real/demanda-real/" },    
+    "Chile": { "color": "#FFCC99", "link": "https://www.coordinador.cl/operacion/graficos/operacion-real/demanda-real/" },   
     "Cyprus": { "color": "#FFDE59", "link": "https://tsoc.org.cy/electrical-system/archive-total-daily-system-generation-on-the-transmission-system/" },
+    "Japan": { "color": "#A23E48", "link": "https://www.tepco.co.jp/en/forecast/html/download-e.html" },
     "Peru": { "color": "#6A0DAD", "link": "https://www.coes.org.pe/Portal/portalinformacion/demanda" },  
     "South Africa": { "color": "#CCFF99", "link": "https://www.eskom.co.za/dataportal/demand-side/" },
     "Turkey": { "color": "#6699FF", "link": "https://www.kaggle.com/datasets/dharanikra/electrical-power-demand-in-turkey" }   
